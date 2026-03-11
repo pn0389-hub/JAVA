805764500364b2ddb317fb89f1ccb0f96c7ea0eb
pn0389-hub <pn0389@srmist.edu.in>
2026-03-11 13:05:59 +0530

    Coded UC1 to Print OOPS to Console

---
+++ OOPSBannerAppUC1.java
@@ -0,0 +1,20 @@
+/**
+ * OOPSBannerAppUC1
+ * 
+ * This program prints the text "OOPS" to the console.
+ * It demonstrates a simple Java program with a main method.
+ * 
+ * @author YourName
+ * @version 1.0
+ */
+
+public class OOPSBannerAppUC1 {
+
+    /**
+     * Main method - Entry point of the Java application
+     * @param args Command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
